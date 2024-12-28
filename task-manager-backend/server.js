69b865e97afd2405662dd7f99946f0de19707cb9
odespaigne <odespaigne@soaintpanama.com>
2024-12-28 11:53:34 -0400

    se agrega variable de entorno -cors

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -11,7 +11,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.ORIGIN_ACCEPT_URL || 'http://localhost:3000'
+}));
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
